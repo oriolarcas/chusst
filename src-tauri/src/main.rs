2bc8176bc6914c3f11073853ed026d60888b49a5
Oriol Arcas <oriol.arcas@gmail.com>
2023-07-25 21:01:48 +0200

    Draw board from Rust data model

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,175 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::Serialize;
+
+#[derive(Copy, Clone, Serialize)]
+enum PieceType {
+    Pawn,   // p
+    Knight, // n
+    Bishop, // b
+    Rook,   // r
+    Queen,  // q
+    King,   // k
+}
+
+#[derive(Copy, Clone, Serialize)]
+enum Player {
+    White,
+    Black,
+}
+
+#[derive(Copy, Clone, Serialize)]
+struct Piece {
+    piece: PieceType,
+    player: Player,
+}
+
+macro_rules! p {
+    (pw) => {
+        Some(Piece {
+            piece: PieceType::Pawn,
+            player: Player::White,
+        })
+    };
+    (nw) => {
+        Some(Piece {
+            piece: PieceType::Knight,
+            player: Player::White,
+        })
+    };
+    (bw) => {
+        Some(Piece {
+            piece: PieceType::Bishop,
+            player: Player::White,
+        })
+    };
+    (rw) => {
+        Some(Piece {
+            piece: PieceType::Rook,
+            player: Player::White,
+        })
+    };
+    (qw) => {
+        Some(Piece {
+            piece: PieceType::Queen,
+            player: Player::White,
+        })
+    };
+    (kw) => {
+        Some(Piece {
+            piece: PieceType::King,
+            player: Player::White,
+        })
+    };
+    (pb) => {
+        Some(Piece {
+            piece: PieceType::Pawn,
+            player: Player::Black,
+        })
+    };
+    (nb) => {
+        Some(Piece {
+            piece: PieceType::Knight,
+            player: Player::Black,
+        })
+    };
+    (bb) => {
+        Some(Piece {
+            piece: PieceType::Bishop,
+            player: Player::Black,
+        })
+    };
+    (rb) => {
+        Some(Piece {
+            piece: PieceType::Rook,
+            player: Player::Black,
+        })
+    };
+    (qb) => {
+        Some(Piece {
+            piece: PieceType::Queen,
+            player: Player::Black,
+        })
+    };
+    (kb) => {
+        Some(Piece {
+            piece: PieceType::King,
+            player: Player::Black,
+        })
+    };
+    () => {
+        Option::<Piece>::None
+    };
+}
+
+#[derive(Copy, Clone, Serialize)]
+struct Board {
+    // rows[x][y], where x = 0..7 = rows 1..8, and y = 0..7 = columns a..h
+    // for instance, e4 is Board.rows[2][4]
+    rows: [[Option<Piece>; 8]; 8],
+}
+
+static BOARD: Board = Board {
+    // Initial board
+    // Note that white pieces are at the top, because arrays are defined top-down, while chess rows go bottom-up
+    rows: [
+        [
+            p!(rw),
+            p!(nw),
+            p!(bw),
+            p!(qw),
+            p!(kw),
+            p!(bw),
+            p!(nw),
+            p!(rw),
+        ],
+        [
+            p!(pw),
+            p!(pw),
+            p!(pw),
+            p!(pw),
+            p!(pw),
+            p!(pw),
+            p!(pw),
+            p!(pw),
+        ],
+        [p!(); 8],
+        [p!(); 8],
+        [p!(); 8],
+        [p!(); 8],
+        [
+            p!(pb),
+            p!(pb),
+            p!(pb),
+            p!(pb),
+            p!(pb),
+            p!(pb),
+            p!(pb),
+            p!(pb),
+        ],
+        [
+            p!(rb),
+            p!(nb),
+            p!(bb),
+            p!(qb),
+            p!(kb),
+            p!(bb),
+            p!(nb),
+            p!(rb),
+        ],
+    ],
+};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_board() -> Board {
+    BOARD
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_board])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -3,16 +3,41 @@ const { invoke } = window.__TAURI__.tauri;
 let greetInputEl;
 let greetMsgEl;
 
-async function greet() {
+async function get_board() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  const board = await invoke("get_board");
+
+  // Object layout:
+  // { rows: [
+  //   0 [{piece: "Rook", player: "White"}, {piece: "Knight", player: "White"}, {piece: "Bishop", player: "White"}, {piece: "Queen", player: "White"}, {piece: "King", player: "White"}, {piece: "Bishop", player: "White"}, {piece: "Knight", player: "White"}, {piece: "Rook", player: "White"}]
+  //   1 [{piece: "Pawn", player: "White"}, {piece: "Pawn", player: "White"}, {piece: "Pawn", player: "White"}, {piece: "Pawn", player: "White"}, {piece: "Pawn", player: "White"}, {piece: "Pawn", player: "White"}, {piece: "Pawn", player: "White"}, {piece: "Pawn", player: "White"}]
+  //   2 [null, null, null, null, null, null, null, null]
+  //   3 [null, null, null, null, null, null, null, null]
+  //   4 [null, null, null, null, null, null, null, null]
+  //   5 [null, null, null, null, null, null, null, null]
+  //   6 [{piece: "Pawn", player: "Black"}, {piece: "Pawn", player: "Black"}, {piece: "Pawn", player: "Black"}, {piece: "Pawn", player: "Black"}, {piece: "Pawn", player: "Black"}, {piece: "Pawn", player: "Black"}, {piece: "Pawn", player: "Black"}, {piece: "Pawn", player: "Black"}]
+  //   7 [{piece: "Rook", player: "Black"}, {piece: "Knight", player: "Black"}, {piece: "Bishop", player: "Black"}, {piece: "Queen", player: "Black"}, {piece: "King", player: "Black"}, {piece: "Bishop", player: "Black"}, {piece: "Knight", player: "Black"}, {piece: "Rook", player: "Black"}]
+  // ] }
+
+  console.log(board);
+
+  const col_id_map = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  for (const [row_index, row_obj] of board.rows.entries()) {
+    for (const [col_index, square] of row_obj.entries()) {
+      const row_id = row_index + 1;
+      const col_id = col_id_map[col_index];
+      console.log(col_id + row_id + ": " + square);
+      let square_element = document.querySelector(".row-" + row_id + " .col-" + col_id + " div");
+      square_element.className = "";
+      if (square !== null) {
+        square_element.classList.add(square.piece.toLowerCase());
+        square_element.classList.add(square.player.toLowerCase());
+      }
+    }
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+  // boardEl = document.querySelector("#board");
+  get_board();
 });
