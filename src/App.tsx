8de4947b4ef68a7d605d0ad4387326ab7a910f0f
Oriol Arcas <oriol.arcas@gmail.com>
2023-07-31 20:02:25 +0200

    Get and draw pieces

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
-import { Component } from 'react';
 import './App.css';
 
+import { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { invoke } from '@tauri-apps/api';
+
+type Square = {
+  piece: string;
+  player: string;
+} | null;
+
+type Position = {
+  row: number;
+  col: number;
+}
+
+type Game = {
+  board: {rows: Square[][]};
+  player: string;
+  turn: number;
+  last_move: Position | null;
+};
 
 class App extends Component<{}, {}> {
-  file_labels(): string[] {
-    return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  state: {game: Game | null} = {game: null};
+
+  constructor(props: any) {
+    super(props);
   }
 
-  column_labels() {
+  async reloadBoard() {
+    const game = await invoke('get_game');
+    this.setState({game});
+  }
+
+  async componentDidMount() {
+    await this.reloadBoard();
+  }
+
+  columnLabels() {
     return (
       <Row className='file-labels m-0'>
       {
-        this.file_labels().map((file =>
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].map((file =>
           <div className='label'>{file}</div>
         ))
       }
@@ -23,11 +52,24 @@ class App extends Component<{}, {}> {
   rank(rank_index: number) {
     return (
       <Row className='rank m-0'>
-        {this.file_labels().map((_, file_index) => {
-          const bg_color = (file_index + rank_index) % 2 == 0 ? 'light' : 'dark';
-          const classes = ['square', bg_color]
+        {Array.from(new Array(8).keys()).map((file_index) => {
+          const bg_color = (file_index + rank_index) % 2 == 0 ? 'dark' : 'light';
+
+          let square_classes = ['square', bg_color];
+          let square = this.state.game?.board.rows[rank_index][file_index];
+          let piece = square?.piece;
+          let player = square?.player;
+          let piece_classes = ['piece'];
+
+          if (piece && player) {
+            piece_classes.push(piece.toLowerCase());
+            piece_classes.push(player.toLowerCase());
+          }
+
           return (
-            <div className={classes.join(' ')}></div>
+            <div className={square_classes.join(' ')}>
+              <div className={piece_classes.join(' ')}></div>
+            </div>
           )
         })}
       </Row>
@@ -41,11 +83,11 @@ class App extends Component<{}, {}> {
         <Row>
           <Col className='px-0'></Col>
           <Col className='p-0'>
-            {this.column_labels()}
+            {this.columnLabels()}
             <div className='board m-0'>
-              {Array.from(Array(8).keys()).map((index) => this.rank(index))}
+              {Array.from(Array(8).keys()).reverse().map((index) => this.rank(index))}
             </div>
-            {this.column_labels()}
+            {this.columnLabels()}
           </Col>
           <Col className='px-0'></Col>
         </Row>
