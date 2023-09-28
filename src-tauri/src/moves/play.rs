108df01e8e434fbfcdad3a191841ba28dda60e04
Oriol Arcas <oriol.arcas@gmail.com>
2023-09-28 19:54:37 +0200

    Reduce unnecessary allocations

--- src-tauri/src/moves/play.rs
+++ src-tauri/src/moves/play.rs
@@ -17,8 +17,6 @@ pub trait PlayableGame<'a> {
     fn as_ref(&self) -> &Game;
     fn as_mut(&mut self) -> &mut Game;
 
-    fn push_moves(&mut self, moves: &mut Vec<ReversableMove>);
-
     fn do_move(&mut self, mv: &Move) -> bool {
         let board = &self.as_ref().board;
 
@@ -53,11 +51,24 @@ pub trait PlayableGame<'a> {
         true
     }
 
-    fn do_move_no_checks(&mut self, mv: &Move) {
+    fn do_move_no_checks(&mut self, mv: &Move);
+}
+
+trait PlayableGameNoChecksImpl<'a>: PlayableGame<'a> {
+    fn do_move_no_checks_impl(
+        &mut self,
+        mv: &Move,
+        moves_option: &mut Option<&mut Vec<ReversableMove>>,
+    ) {
         let board = &mut self.as_mut().board;
 
         let square_opt = board.square(&mv.source).as_ref();
-        assert!(square_opt.is_some(), "move {} from empty square:\n{}", mv, board);
+        assert!(
+            square_opt.is_some(),
+            "move {} from empty square:\n{}",
+            mv,
+            board
+        );
         let square = square_opt.unwrap();
 
         let player = square.player;
@@ -87,12 +98,13 @@ pub trait PlayableGame<'a> {
             }
             _ => MoveExtraInfo::Other,
         };
-        let mut moves: Vec<ReversableMove> = Vec::new();
 
-        moves.push(ReversableMove {
-            mv: *mv,
-            previous_piece: *board.square(&mv.target),
-        });
+        if let Some(moves) = moves_option {
+            moves.push(ReversableMove {
+                mv: *mv,
+                previous_piece: *board.square(&mv.target),
+            });
+        }
 
         board.move_piece(&mv.source, &mv.target);
 
@@ -107,36 +119,44 @@ pub trait PlayableGame<'a> {
                     only_enemy(&board, try_move(&mv.target, &dir!(-direction, 0)), &player)
                         .unwrap();
 
-                moves.push(ReversableMove {
-                    mv: mv!(passed, passed),
-                    previous_piece: *board.square(&passed),
-                });
+                if let Some(moves) = moves_option {
+                    moves.push(ReversableMove {
+                        mv: mv!(passed, passed),
+                        previous_piece: *board.square(&passed),
+                    });
+                }
 
                 board.update(&passed, None);
             }
             MoveExtraInfo::Promotion(piece) => {
-                moves.push(ReversableMove {
-                    mv: mv!(mv.target, mv.target),
-                    previous_piece: *board.square(&mv.target),
-                });
+                if let Some(moves) = moves_option {
+                    moves.push(ReversableMove {
+                        mv: mv!(mv.target, mv.target),
+                        previous_piece: *board.square(&mv.target),
+                    });
+                }
                 board.update(&mv.target, Some(Piece { piece, player }));
             }
             MoveExtraInfo::CastleKingside => {
                 let rook_source = try_move(&mv.source, &dir!(0, 3)).unwrap();
                 let rook_target = try_move(&mv.source, &dir!(0, 1)).unwrap();
-                moves.push(ReversableMove {
-                    mv: mv!(rook_source, rook_target),
-                    previous_piece: None,
-                });
+                if let Some(moves) = moves_option {
+                    moves.push(ReversableMove {
+                        mv: mv!(rook_source, rook_target),
+                        previous_piece: None,
+                    });
+                }
                 board.move_piece(&rook_source, &rook_target);
             }
             MoveExtraInfo::CastleQueenside => {
                 let rook_source = try_move(&mv.source, &dir!(0, -4)).unwrap();
                 let rook_target = try_move(&mv.source, &dir!(0, -1)).unwrap();
-                moves.push(ReversableMove {
-                    mv: mv!(rook_source, rook_target),
-                    previous_piece: None,
-                });
+                if let Some(moves) = moves_option {
+                    moves.push(ReversableMove {
+                        mv: mv!(rook_source, rook_target),
+                        previous_piece: None,
+                    });
+                }
                 board.move_piece(&rook_source, &rook_target);
             }
             _ => (),
@@ -154,7 +174,6 @@ pub trait PlayableGame<'a> {
         }
 
         self.as_mut().player = enemy(&self.as_ref().player);
-        self.push_moves(&mut moves);
         self.as_mut().last_move = Some(MoveInfo {
             mv: *mv,
             info: move_info,
@@ -183,8 +202,12 @@ impl<'a> PlayableGame<'a> for SearchableGame<'a> {
         &mut self.game
     }
 
-    fn push_moves(&mut self, _moves: &mut Vec<ReversableMove>) {}
+    fn do_move_no_checks(&mut self, mv: &Move) {
+        self.do_move_no_checks_impl(mv, &mut None)
+    }
 }
+
+impl<'a> PlayableGameNoChecksImpl<'a> for SearchableGame<'a> {}
 
 pub struct ReversableGame<'a> {
     game: &'a mut Game,
@@ -213,10 +236,15 @@ impl<'a> PlayableGame<'a> for ReversableGame<'a> {
         &mut self.game
     }
 
-    fn push_moves(&mut self, moves: &mut Vec<ReversableMove>) {
-        self.moves.append(moves);
+    fn do_move_no_checks(&mut self, mv: &Move) {
+        let mut moves: Vec<ReversableMove> = Vec::new();
+        let result = self.do_move_no_checks_impl(mv, &mut Some(&mut moves));
+        self.moves.append(&mut moves);
+        result
     }
 }
+
+impl<'a> PlayableGameNoChecksImpl<'a> for ReversableGame<'a> {}
 
 impl<'a> ReversableGame<'a> {
     #[allow(dead_code)]
